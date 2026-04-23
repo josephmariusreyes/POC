b8c197520a1bc7c62bcf7362d1865e7390e6a320
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-04-23 09:28:57 +0800

    show back again root landing page

--- poc-auth-apis/demo-sanctum-api/routes/web.php
+++ poc-auth-apis/demo-sanctum-api/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 // This endpoint not needed since I am working on an API
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
