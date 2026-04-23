b801fa8b8cac1c6e3ea2292e6e91ee2614341b4b
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-04-24 02:31:06 +0800

    DB seeder updates

--- poc-auth-apis/demo-sanctum-api/database/seeders/DatabaseSeeder.php
+++ poc-auth-apis/demo-sanctum-api/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call(RoleSeeder::class);
+        Role::factory()->superAdmin()->create();
+        Role::factory()->eventOrganizer()->create();
+        Role::factory()->queAdmin()->create();
+        Role::factory()->queEncoder()->create();
+
+        User::factory()
+            ->count(1)
+            ->withRole('SuperAdmin')
+            ->create();
+
+        User::factory()
+            ->count(1)
+            ->withRole('EventOrganizer')
+            ->create();
+
+        User::factory()
+            ->count(1)
+            ->withRole('QueAdmin')
+            ->create();
     }
 }
