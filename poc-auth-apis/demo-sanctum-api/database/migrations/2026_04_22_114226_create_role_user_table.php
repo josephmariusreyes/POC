a929bb9c7d630fc38cdabb5b60bd168e73384807
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-04-25 17:54:47 +0800

    migration DB updates for may pila app

--- poc-auth-apis/demo-sanctum-api/database/migrations/2026_04_22_114226_create_role_user_table.php
+++ poc-auth-apis/demo-sanctum-api/database/migrations/2026_04_22_114226_create_role_user_table.php
@@ -5,15 +5,16 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
+    
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->foreignId('user_id')
-                  ->constrained()
+                  ->constrained('users')
                   ->cascadeOnDelete();
 
             $table->foreignId('role_id')
-                  ->constrained()
+                  ->constrained('roles')
                   ->cascadeOnDelete();
 
             $table->primary(['user_id', 'role_id']);
