5f922d70a9c1cd3b5437f6c94779c0ebd8fdfe0a
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-04-27 17:59:13 +0800

    app models

--- poc-auth-apis/demo-sanctum-api/app/Models/Role.php
+++ poc-auth-apis/demo-sanctum-api/app/Models/Role.php
@@ -13,4 +13,23 @@ class Role extends Model
     {
         return $this->belongsToMany(User::class);
     }
+
+    // NOTE: in scenarios that the pivot table did not follow the larvel naming convention
+    // we must explicitly tell laravel the table name
+    
+    // public function users()
+    // {
+    //     return $this->belongsToMany(User::class, 'pivot.role_user');
+    // }    
+
+    // Also below is sample if it did not follow the convention for naming the columns
+    // public function roles()
+    // {
+    // return $this->belongsToMany(
+    //     User::class,
+    //     'pivot.role_user',
+    //     'customer_user_id',  // FK pointing to users table
+    //     'customer_role_id'   // FK pointing to roles table
+    // );
+    // }   
 }
