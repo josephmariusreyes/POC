6f759e813660c84a24ff09deee4006e63ebf578c
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-06-07 10:08:38 +0800

    Remove not used auth.guard

--- poc-vue/demo-crm/src/features/auth/guards/auth.guard.ts
+++
@@ -1,7 +0,0 @@
-import type { RouteLocationNormalized } from 'vue-router'
-
-import { canAccessRoute } from '@/app/guards/auth.guard'
-
-export function authGuard(to: RouteLocationNormalized) {
-	return canAccessRoute(to)
-}
